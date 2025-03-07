54a39f7a3ed079504d0b5c270e2295f6ecb6be30
sumudusc <126461991+sumudusc@users.noreply.github.com>
2025-03-07 11:05:50 +0000

    Initial Monad project

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,12 +3,6 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
+    function setNumber(uint256 newNumber) public { number = newNumber; }
+    function increment() public { number++; }
 }
